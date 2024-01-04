d8cbc4c0e9a053a2181b7d8f8466ddd4e759350f
eduard <avas.eduard@gmail.com>
2024-01-04 21:03:07 +0200

    created the summarize function

---
+++ summarize.py
@@ -0,0 +1,36 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Create the OpenAI client with the API key
+client = OpenAI(api_key = os.getenv('OPENAI_API_KEY'))
+
+# Summarize the transcribed text using ChatGPT
+def summarize_text(chunks, output_file=None):
+    summaries = []
+    # Loop through each chunk of text and create a chat using OpenAI's API
+    for chunk in chunks:
+        completion = client.chat.completions.create(
+            model = 'gpt-3.5-turbo',
+            messages = [
+              {'role': 'system', 'content': 'You are a helpful assistant that summarizes youtube videos. You are provided chunks of raw audio that were transcribed from a youtube video. Summarize the current chunk to succint and clear bullet points of its contents.'},
+              {'role': 'user', 'content': chunk}
+            ]
+          )
+        # Extract the summary from the response
+        summary = completion.choices[0].message.content
+        # Append the summary to the list of summaries
+        summaries.append(summary)
+    # If an output_file is specified, save all summaries to a .txt file
+    if output_file is not None:
+        with open(output_file, 'w') as file:
+            for summary in summaries:
+                file.write(summary + '\n')
+    # Return the list of summaries
+    return summaries
+
+# Test run
+# summarize_text(['text_file'], 'summary')
--- transcribe.py
+++ transcribe.py
@@ -1,15 +1,12 @@
-from dotenv import load_dotenv
-from openai import OpenAI
 import os
+from openai import OpenAI
+from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Get your OpenAI API key from the environment variables
-api_key = os.getenv("OPENAI_API_KEY")
-
 # Create the OpenAI client with the API key
-client = OpenAI(api_key=api_key)
+client = OpenAI(api_key = os.getenv('OPENAI_API_KEY'))
 
 # Transcribe the audio files of the video
 def transcribe_audio_files(audio_files, output_file=None):
@@ -20,13 +17,12 @@ def transcribe_audio_files(audio_files, output_file=None):
         audio = open(audio_file, 'rb')
         # Use OpenAI API to transcribe the audio using Whisper
         transcript = client.audio.transcriptions.create(
-            model="whisper-1", 
-            file=audio, 
-            response_format="text"
+            model='whisper-1',
+            file=audio,
+            response_format='text'
           )
         # Append the transcribed text to the transcripts list
         transcripts.append(transcript)
-
     # If an output file is specified, save all transcripts to a .txt file
     if output_file is not None:
         with open(output_file, 'w') as file:
